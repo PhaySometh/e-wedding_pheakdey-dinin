b3d932b868af492f86ec01b4446aacbcc22d265f
Phay Someth <phay.someth70@gmail.com>
2026-01-15 14:47:59 +0700

    style: Format imageSizes array for better readability

--- next.config.js
+++ next.config.js
@@ -5,7 +5,9 @@ const nextConfig = {
     unoptimized: false,
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 512, 640, 768, 1024, 1280, 1536],
+    imageSizes: [
+      16, 32, 48, 64, 96, 128, 256, 384, 512, 640, 768, 1024, 1280, 1536,
+    ],
     minimumCacheTTL: 60 * 60 * 24 * 365, // 1 year
     remotePatterns: [],
   },
